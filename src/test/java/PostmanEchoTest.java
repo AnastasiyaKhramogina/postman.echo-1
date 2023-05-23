6bb0c26e15704ea39fc1685eeb311636e36d04d4
Anastasya Khramogina <akhramogina@mail.ru>
2023-05-23 14:04:25 +0300

    failed commt

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnPostedData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(100)
+                .body ("data", equalTo("some data"))
+        ;
+    }
+}
