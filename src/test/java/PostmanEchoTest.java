96c44b5bcad1b57722947b17450069bb85230f3f
Anastasya Khramogina <akhramogina@mail.ru>
2023-05-23 14:08:37 +0300

    sucess commit

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -15,7 +15,7 @@ public class PostmanEchoTest {
                 .when()
                 .post("/post")
                 .then()
-                .statusCode(100)
+                .statusCode(200)
                 .body ("data", equalTo("some data"))
         ;
     }
